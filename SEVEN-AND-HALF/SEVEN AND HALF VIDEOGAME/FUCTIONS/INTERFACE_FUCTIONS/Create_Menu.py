3faa5a5faf1b38ca59b1292cf66b791673de4bb3
Miguel Ángel López <mangell0624@gmail.com>
2025-01-09 18:10:45 +0100

    Fuction create menus
    Se agrego la función para crear menús.

--- SEVEN-AND-HALF/SEVEN AND HALF VIDEOGAME/FUCTIONS/INTERFACE_FUCTIONS/Create_Menu.py
+++ SEVEN-AND-HALF/SEVEN AND HALF VIDEOGAME/FUCTIONS/INTERFACE_FUCTIONS/Create_Menu.py
@@ -1,1 +1,28 @@
 #Funcion para crear menus.
+def make_menu(options,title="",space=40,symbol="",distribution="Distinct"):
+    while True:
+        print()
+        if symbol != "" or title != "" or space <= 0:
+            if type(title) == str:
+                if distribution == "Distinct":
+                    print("".center(space,symbol))
+                    print(title.center(space))
+                    print("".center(space,symbol))
+                else:
+                    print(title.center(space,symbol))
+            else:
+                print("".center(space,symbol))
+                for i in range(len(title)):
+                    print(title[i])
+                print("".center(space,symbol))
+        for i in range(len(options)):
+            text = str(i+1) + ") " + options[i]
+            print(text.center(space))
+        opc = input("Option: ".rjust(space//2+5))
+        if not opc.isdigit():
+            print("\nThe option is can only digit.")
+        elif not int(opc) in range(1,len(options)+1):
+            print("\nThe option out of range.")
+        else:
+            return opc
+        input("\nEnter to continue")
--- SEVEN-AND-HALF/SEVEN AND HALF VIDEOGAME/SEVEN-AND-HALF.py
+++ SEVEN-AND-HALF/SEVEN AND HALF VIDEOGAME/SEVEN-AND-HALF.py
@@ -1,1 +1,8 @@
 #Archivo Base del video juego.
+
+#Funciones importadas
+import FUCTIONS.INTERFACE_FUCTIONS.Create_Menu as Create_Menus
+
+opc = Create_Menus.make_menu(("1","2","3","4","5","6"),title="Hola_prueba",space=40,symbol="*")
+if opc == 1:
+    print(1)
