4095c38376e0fef99edb794afb2bfdd44788ecce
Miguel Ángel López <mangell0624@gmail.com>
2025-01-10 09:33:24 +0100

    Primer menu y mejoras en la funcion make_menu
    Se a mejorado el funcionamiento de los menús con texto ASCII y se a añadido el titulo del juego "Seven and half" en texto ASCII  también se a terminado de hacer todo el primer menú.

--- SEVEN-AND-HALF/SEVEN AND HALF VIDEOGAME/FUCTIONS/INTERFACE_FUCTIONS/Create_Menu.py
+++ SEVEN-AND-HALF/SEVEN AND HALF VIDEOGAME/FUCTIONS/INTERFACE_FUCTIONS/Create_Menu.py
@@ -1,1 +1,35 @@
 #Funcion para crear menus.
+def make_menu(options,title="",space=40,symbol="",distribution="Distinct",secundary_symbol=""):
+    while True:
+        print()
+        if symbol != "" or title != "" or space <= 0:
+            if type(title) == str:
+                if distribution == "Distinct":
+                    print("".center(space,symbol))
+                    print(title.center(space))
+                    print("".center(space,symbol))
+                else:
+                    print(title.center(space,symbol))
+            else:
+                if distribution == "Distinct":
+                    for i in range(len(title)):
+                        print(title[i].center(space,symbol))
+                else:
+                    print("".center(space,symbol))
+                    for i in range(len(title)):
+                        print(title[i].center(space))
+                    print()
+                    print("".center(space,symbol))
+                    print()
+        for i in range(len(options)):
+            text = str(i+1) + ") " + options[i]
+            print("".rjust(space//2-10) + text)
+        opc = input("".rjust(space//2-10) + "Option: ")
+        print()
+        if not opc.isdigit():
+            print("Invalid Option".center(space,secundary_symbol))
+        elif not int(opc) in range(1,len(options)+1):
+            print("Invalid Option".center(space,secundary_symbol))
+        else:
+            return int(opc)
+        input("".rjust(space//2-10) + "Press enter to continue")
--- SEVEN-AND-HALF/SEVEN AND HALF VIDEOGAME/SEVEN-AND-HALF.py
+++ SEVEN-AND-HALF/SEVEN AND HALF VIDEOGAME/SEVEN-AND-HALF.py
@@ -1,1 +1,46 @@
 #Archivo Base del video juego.
+
+#Funciones importadas
+import FUCTIONS.INTERFACE_FUCTIONS.Create_Menu as Create_Menus
+
+#Variables
+title_game = (
+    "  _____                          ___              __   __  __      ______",
+    " / ___/ ___ _   _____  ____     /   |  ____  ____/ /  / / / /___ _/ / __/ ",
+    " \\__ \\_/ _ \\ | / / _ \\/ __ \\   / /| | / __ \\/ __  /  / /_/ / __ `/ / /_   ",
+    " ___/ /  __/ |/ /  __/ / / /  / ___ |/ / / / /_/ /  / __  / /_/ / / __/   ",
+    "/____/\\___/|___/\\___/_/ /_/  /_/  |_/_/ /_/\\__,_/  /_/ /_/\\__,_/_/_/     "
+)
+education_center = (
+    " ______     _                   _______                      _             _   _ _ _",
+    "|  ____|   | |                 |__   __|                    | |           (_) (_) | |",
+    "| |__   ___| |_ _____   _____     | | ___ _ __ _ __ __ _  __| | __ _ ___   _   _| | | __ _",
+    "|  __| / __| __/ _ \\ \\ / / _ \\    | |/ _ \\ '__| '__/ _` |/ _` |/ _` / __| | | | | | |/ _` |",
+    "| |____\\__ \\ ||  __/\\ V /  __/    | |  __/ |  | | | (_| | (_| | (_| \\__ \\ | | | | | | (_| |",
+    "|______|___/\\__\\___| \\_/ \\___|    |_|\\___|_|  |_|  \\__,_|\\__,_|\\__,_|___/ |_| |_|_|_|\\__,_|"
+)
+
+#Opciones
+basic_options = ("Add/Remove/Show Players","Settings","Play Game","Ranking","Reports","Exit")
+
+#Flags
+flg_terminar = False
+flg_00 = True
+
+while not flg_terminar:
+
+    while flg_00:
+        opc = Create_Menus.make_menu(options=basic_options,title=title_game,space=200,symbol="*",distribution="A",secundary_symbol="=")
+        if opc == 1:
+            print(1)
+        elif opc == 2:
+            print(2)
+        elif opc == 3:
+            print(3)
+        elif opc == 4:
+            print(4)
+        elif opc == 5:
+            print(5)
+        else:
+            flg_00 = False
+            flg_terminar = True
