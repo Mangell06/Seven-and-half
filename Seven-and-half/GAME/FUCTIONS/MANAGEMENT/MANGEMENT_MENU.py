d38383d48d4181e509d298b4aac900f4aa87644a
Miguel Ángel López <mangell0624@gmail.com>
2025-01-13 16:11:31 +0100

    Update a función del menu
    Tras una revisión se encontró con que no era correcto la función de gestionar el menú y se a actualizado.

--- Seven-and-half/GAME/FUCTIONS/MANAGEMENT/MANGEMENT_MENU.py
+++ Seven-and-half/GAME/FUCTIONS/MANAGEMENT/MANGEMENT_MENU.py
@@ -2,13 +2,11 @@ def menuopciones(texto_opcion="",introducir_opcion="",rango_lista=[],excepciones
     while True:
         if texto_opcion != "":
             print(texto_opcion.center(50))
-        introducir_opcion = input("Option: ".center(50))
-        if introducir_opcion in excepciones:
-            error = "La opcion " + introducir_opcion + " no esta permitida.\nVuelva a intentarlo."
-            print(error.center(50))
-        elif introducir_opcion in rango_lista:
-            return introducir_opcion
+        opc = input(introducir_opcion)
+        if not opc.isdigit():
+            print("Invalid Option".center(50,"-"))
+        if opc in rango_lista or opc in excepciones:
+            return opc
         else:
-            error = "La opcion " + introducir_opcion + " no esta permitida.\nVuelva a intentarlo."
-            print(error.center(50))
+            print("Invalid Option".center(50,"-"))
         input("Press enter to continue")
