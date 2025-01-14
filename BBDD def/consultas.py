7c6aa02bde8cdcd9e7a4c78dde00c28aeae476ba
Nazan <naguilarperez.cf@iesesteveterradas.cat>
2025-01-14 19:39:55 +0100

    Actualizacion

--- BBDD def/consultas.py
+++ BBDD def/consultas.py
@@ -1,11 +1,10 @@
 import pymysql
-
 from conexion import connect_to_database, close_connection
 
 def execute_query(connection, query):
     """Ejecuta una consulta SQL y devuelve los resultados."""
     try:
-        with connection.cursor() as cursor:
+        with connection.cursor(pymysql.cursors.DictCursor) as cursor:  # Usar DictCursor
             cursor.execute(query)
             results = cursor.fetchall()
             return results
@@ -15,24 +14,22 @@ def execute_query(connection, query):
 
 
 def get_personajes():
-    """Consulta la tabla de Personajes y devuelve los resultados."""
+    """Consulta la tabla de Personajes y devuelve los resultados como un diccionario."""
     query = "SELECT * FROM personajes;"  # Consulta a la tabla Personajes
     connection = connect_to_database()
 
     if connection:
         results = execute_query(connection, query)
+        close_connection(connection)  # Asegúrate de cerrar la conexión después de la consulta
         if results:
-            for row in results:
-                print(row)  # Muestra cada fila (registro) en la tabla
-        close_connection(connection)
+            # Crear un diccionario usando 'id' como clave
+            personajes_dict = {row['ID']: {"Name": row['Name'], "Risk": row['Risk'], "Type": row["Type"], "Puntos": row["Puntos"], "Minutos_Jugados": row["Minutos_Jugados"], "In_Game": row["In_Game"]} for row in results}
+            return personajes_dict  # Devuelve el diccionario
+    return {}  # Devuelve un diccionario vacío si no hay resultados o hay un error
 
 
-if __name__ == "__main__":
-    # Llamamos a la función que consulta los personajes
-    get_personajes()
+# Guardar los resultados en un diccionario
+personajes_dict = get_personajes()
 
-
-# Type = 1 = Cauteloso
-# Type = 2 = Normal
-# Type = 3 = Agresivo
-
+# Usar la variable 'personajes_dict' para lo que necesites
+print(personajes_dict)  # Muestra el diccionario
